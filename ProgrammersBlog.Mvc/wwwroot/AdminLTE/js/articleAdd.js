60c2b328be26c1ea9b2e9bcc8b3dd293414e34f8
Muzaffer Erdoğan <muzaffer45e@outlook.com>
2021-09-23 12:20:02 +0300

    continue

---
+++ ProgrammersBlog.Mvc/wwwroot/AdminLTE/js/articleAdd.js
@@ -0,0 +1,66 @@
+﻿$(document).ready(function () {
+
+    //trumbowyg start 
+    $('#text-editor').trumbowyg({
+        btns: [
+            ['viewHTML'],
+            ['undo', 'redo'], // Only supported in Blink browsers
+            ['formatting'],
+            ['strong', 'em', 'del'],
+            ['superscript', 'subscript'],
+            ['link'],
+            ['insertImage'],
+            ['justifyLeft', 'justifyCenter', 'justifyRight', 'justifyFull'],
+            ['unorderedList', 'orderedList'],
+            ['horizontalRule'],
+            ['removeformat'],
+            ['fullscreen'],
+            ['foreColor', 'backColor'],
+            ['emoji'],
+            ['fontfamily'],
+            ['fontsize']
+        ],
+        lang: "tr"
+    });
+
+    //trumbowyg end
+
+    //select2 start
+    $('#categoryList').select2({
+        theme: 'bootstrap4',
+        placeholder: "Lütfen bir kategori seçiniz...",
+        allowClear: true
+    });
+
+    //select2 end
+
+    //jquery uı datepicker start
+    $(function () {
+        $("#datepicker").datepicker({
+            closeText: "kapat",
+            prevText: "&#x3C;geri",
+            nextText: "ileri&#x3e",
+            currentText: "bugün",
+            monthNames: ["Ocak", "Şubat", "Mart", "Nisan", "Mayıs", "Haziran",
+                "Temmuz", "Ağustos", "Eylül", "Ekim", "Kasım", "Aralık"],
+            monthNamesShort: ["Oca", "Şub", "Mar", "Nis", "May", "Haz",
+                "Tem", "Ağu", "Eyl", "Eki", "Kas", "Ara"],
+            dayNames: ["Pazar", "Pazartesi", "Salı", "Çarşamba", "Perşembe", "Cuma", "Cumartesi"],
+            dayNamesShort: ["Pz", "Pt", "Sa", "Ça", "Pe", "Cu", "Ct"],
+            dayNamesMin: ["Pz", "Pt", "Sa", "Ça", "Pe", "Cu", "Ct"],
+            weekHeader: "Hf",
+            dateFormat: "dd.mm.yy",
+            firstDay: 1,
+            isRTL: false,
+            showMonthAfterYear: false,
+            yearSuffix: "",
+            duration: 750,
+            showAnim: "drop",
+            showOptions: {direction:"left"},
+            minDate: -3,
+            maxDate: +3
+        });
+    });
+    //jquery uı datepicker end
+
+});
